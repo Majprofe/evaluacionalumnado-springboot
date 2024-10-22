332affd8a836270add27b8b31a3e90e86e5b1c2a
majprofe <marrjua393@g.educaand.es>
2024-10-22 18:44:17 +0200

    Commit inicial

---
+++ src/main/java/es/iejandula/evaluacionalumnado/EvaluacionalumnadoApplication.java
@@ -0,0 +1,13 @@
+package es.iejandula.evaluacionalumnado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionalumnadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionalumnadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/iejandula/evaluacionalumnado/EvaluacionalumnadoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iejandula.evaluacionalumnado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionalumnadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
